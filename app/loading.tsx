1a41d32c68cc559e479e5d5730518c92454b96f8
Md. Nesad <131846011+Md-Nesad@users.noreply.github.com>
2025-05-21 20:27:38 +0600

    Done for now

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default async function LoadingPage() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div className='p-6 rounded-lg shadow-md w-1/3 text-center'>
+        <h1 className='text-2xl font-bold'>Loading....</h1>
+      </div>
+    </div>
+  )
+}
